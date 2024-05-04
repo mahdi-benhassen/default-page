b4a322362bf7406580b48dba2dd98d3084c4f26e
Mahdi BEN HASSEN <mahdi.ben-hassen@sagemcom.com>
2024-05-04 22:10:12 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const changeTextButton = document.getElementById("changeTextButton");
-const paragraph = document.getElementById("myParagraph");
-
-changeTextButton.addEventListener("click", function() {
-  paragraph.textContent = "The text has changed!";
-});
